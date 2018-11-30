d0d62ce418cd77a889f33bb0992e8d61965d233e
Ilich Arredondo <ilich1@hotmail.com>
2018-11-29 19:49:53 -0600

    Progreso inicial de la clase-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
+import { PersonasModule } from './personas/personas.module';
 
 import { AppComponent } from './app.component';
 import { Componente1Component } from './componente1/componente1.component';
-import { EditorPersonasComponent } from './editor-personas/editor-personas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Componente1Component,
-    EditorPersonasComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    PersonasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/personas/personas.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { EditorPersonasComponent } from './editor-personas/editor-personas.component';
+
+@NgModule({
+  declarations: [
+    EditorPersonasComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    EditorPersonasComponent
+  ]
+})
+export class PersonasModule { }
